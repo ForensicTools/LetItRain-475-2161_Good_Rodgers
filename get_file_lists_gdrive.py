8dc974083274c0deb4c6eba32e0db26192a487ea
mgood15 <meghan@csh.rit.edu>
2016-10-20 22:48:33 -0400

    Added to the revision code to allow for downloading files' past revisions. Also wrote a download_file function that has the beginnings for downloading specified files.

--- get_file_lists_gdrive.py
+++ get_file_lists_gdrive.py
@@ -3,12 +3,17 @@
 
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+import httplib2
+import json
 
 # Handles authentication
 def auth():
     gauth = GoogleAuth()
     gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
-    return gauth
+    http = gauth.Get_Http_Object()
+    lst = [gauth, http]
+    return lst
+
 
 # Retrieves the information about every file
 # Can either do deleted or regular files
@@ -25,24 +30,31 @@ def list_files(gauth, trashed):
 # Retrieves version information in JSON format of previous versions
 # given a file ID
 def get_file_versions(fileID, gauth):
-    http = gauth[1]
+    http = gauth
     url = "https://www.googleapis.com/drive/v3/files/" + fileID + "/revisions"
     req = http.request(url, 'GET')
-
-    print req[1]
     res = json.loads(req[1])
-    print "\n\n" + res["revisions"][0]["id"]
-    print len(res["revisions"])
+    print("There are " + str(len(res["revisions"])) + " revisions for this file.")
 
-# Download files from drive
-# TODO
+    #fileID_for_revision will be the fileID of the revision in question
+    url2 = url + "/" + fileID_for_revision + "?alt=media"
+    response, content = http.request(url2, 'GET')
+    with open("test.docx", "wb") as code:
+        code.write(content)
+
+# Download files from drive when given the fileID
+def download_file(gauth, fileID):
+    drive = GoogleDrive(gauth)
+    file1 = drive.CreateFile({'id': fileID})
+    mime = file1['mimeType']
+    content = file1.GetContentFile(file1['title'])
 
 def main():
     output_file = open('file_list.txt', 'w')
     trashed_output_file = open('trashed_file_list.txt', 'w')
     gauth = auth()
-    file_list = list_files(gauth, False)
-    trashed_file_list = list_files(gauth, True)
+    file_list = list_files(gauth[0], False)
+    trashed_file_list = list_files(gauth[0], True)
     for file_entry in file_list:
         output_file.write("{}\n".format(file_entry['title']))
     print("List of regular files has been saved as file_list.txt")
